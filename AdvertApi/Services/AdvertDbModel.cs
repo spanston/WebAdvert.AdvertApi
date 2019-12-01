3a25940ee606f525871dbdb0a4a7a02d2116c6ba
Johannes Chamuon <jcn16005@student.mdh.se>
2019-12-01 09:20:09 +0100

    AdvertAPI

---
+++ AdvertApi/Services/AdvertDbModel.cs
@@ -0,0 +1,25 @@
+﻿using AdvertApi.Models;
+using Amazon.DynamoDBv2.DataModel;
+using System;
+
+
+namespace AdvertApi.Services
+{
+    [DynamoDBTable("Advert")]
+    public class AdvertDbModel
+    {
+        [DynamoDBHashKey]
+        public string Id { get; set; }
+        [DynamoDBHashKey]    
+        public string Title { get; set; }
+        [DynamoDBProperty]
+        public string Description { get; set; }
+        [DynamoDBProperty]
+        public double Price { get; set; }
+        [DynamoDBProperty]
+        public DateTime CreationDateTime { get; set; }
+        [DynamoDBProperty] 
+        public AdvertStatus Status { get; set; }        
+
+    }
+}
---
+++ AdvertApi/Services/AdvertProfile.cs
@@ -0,0 +1,14 @@
+﻿using AdvertApi.Models;
+using AutoMapper;
+
+namespace AdvertApi.Services
+{
+    public class AdvertProfile : Profile 
+    {
+        //GOING TO MAP AFTER ATTRIBUTE NAME
+        public AdvertProfile()
+        {
+            CreateMap<AdvertModel, DynamoDbAdvertStorage>();
+        }
+    }
+}
